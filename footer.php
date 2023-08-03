0ab8811d3bc7d87dc6b75e955d29c89d0a4b632e
Cristian Berrios <cristian@ilogica.cl>
2023-08-02 23:52:55 -0400

    fixes

--- footer.php
+++ footer.php
@@ -4,33 +4,44 @@
 
 <?php do_action( 'tailpress_content_after' ); ?>
 
-<footer class="bg-blue-950 text-white py-10">
+<footer class="bg-gray-200 text-gray-900 py-10">
 
-	<div class="container mx-auto grid grid-cols-4 gap-5">
+	<div class="container mx-auto">
+		<div class="flex items-start items-center gap-4">
+			<div class="logo">
+				<img src="https://funerariaazocar.cl/wp-content/uploads/2017/12/logo-funeraria.png" alt="">
+			</div>
+			<div class="contents flex-1">
+				<div class="flex justify-start gap-2">
+					<a href="#">Términos y condiciones</a>
+					<a href="#">Términos y condiciones</a>
+					<a href="#">Términos y condiciones</a>
+				</div>
+				<div>
+					<p>Funeraria Azócar - 2023</p>
+				</div>
+				<div class="social flex justify-end gap-4">
+					<a href="#">
+						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
+						</svg>
+					</a>
+					<a href="#">
+						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
+						</svg>
+					</a>
+					<a href="#">
+						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
+						</svg>
+					</a>
+				</div>
 
-		<div class="logo">
-			<img src="https://funerariaazocar.cl/wp-content/uploads/2017/12/logo-funeraria.png" alt="">
+			</div>
 		</div>
-
-		<div>
-			<h3 class="text-2xl">Dirección</h3>
-			<p>Bezanilla 1330, Independencia, Santiago de Chile</p>
-			<p>Correo:comercial@funerariaazocar.cl</p>
-		</div>
-
-		<div>
-			<h3 class="text-2xl">Ventas</h3>
-			<p>22 364 6000</p>
-			<p>22 737 0533</p>
-
-			<h3 class="text-2xl">Teléfono Móvil</h3>
-			<p>+569 9 330 4778</p>
-			<p>+569 9 818 8978</p>
-		</div>
-
-		<div>
-			<h3 class="text-2xl">Redes sociales</h3>
-		</div>
+		
+		
 
 	</div>
 
