22db512811ad2e22f67f1923ce2070ec19f29421
Cristian Berrios <cristian@ilogica.cl>
2023-08-07 18:34:48 -0400

    header links

--- header.php
+++ header.php
@@ -25,7 +25,7 @@
 	<header class="bg-gray-50 border-b-1 border-blue-950 shadow-md sticky top-0 z-50">
 		<div class="container mx-auto flex justify-between align-top">
 			<div class="logo py-2">
-				<a href="#" class="block hover:opacity-60 transition-all">
+				<a href="<?php echo home_url(); ?>" class="block hover:opacity-60 transition-all">
 					<img src="<?php bloginfo('template_url'); ?>/images/logo-horizontal.png" alt="Funeraria Azócar" class="w-auto max-h-16 md:max-h-20">
 				</a>
 			</div>
@@ -33,29 +33,35 @@
 			<div class="row hidden md:block">
 				<nav class="secondary-menu">
 					<ul>
-						<li><a href="#">Conócenos</a></li>
-						<li><a href="#">Repatriaciones</a></li>
-						<li><a href="#">Convenios</a></li>
-						<li><a href="#">Beneficios</a></li>						
-						<li><a href="#">Encuentrános</a></li>						
+						<li><a href="<?php echo home_url(); ?>/nosotros">Conócenos</a></li>
+						<li><a href="<?php echo home_url(); ?>/repatriaciones">Repatriaciones</a></li>
+						<li><a href="<?php echo home_url(); ?>/convenios">Convenios</a></li>
+						<li><a href="<?php echo home_url(); ?>/beneficios">Beneficios</a></li>						
+						<li><a href="<?php echo home_url(); ?>/encuentranos">Encuéntranos</a></li>						
 					</ul>
 				</nav>
 
 				<nav class="main-menu">
 					<ul>
 						<li>
-							<a href="#">Necesito un servicio</a>
+							<a href="<?php echo home_url(); ?>/servicios">Necesito un servicio</a>
 							<ul>
-								<li><a href="#">Memorial</a></li>
-								<li><a href="#">Eternal</a></li>
-								<li><a href="#">Clásico</a></li>
-								<li><a href="#">Tradicional</a></li>
-								<li><a href="#">Estándar</a></li>
+								<li><a href="<?php echo home_url(); ?>/servicios/memorial">Memorial</a></li>
+								<li><a href="<?php echo home_url(); ?>/servicios/eternal">Eternal</a></li>
+								<li><a href="<?php echo home_url(); ?>/servicios/clasico">Clásico</a></li>
+								<li><a href="<?php echo home_url(); ?>/servicios/tradicional">Tradicional</a></li>
+								<li><a href="<?php echo home_url(); ?>/servicios/estandar">Estándar</a></li>
 							</ul>
 						</li>
-						<li><a href="#">Planificar en vida</a></li>
-						<li><a href="#">Qué hacer después</a></li>
-						<li><a href="#">Contáctanos</a></li>
+						<li><a href="<?php echo home_url(); ?>/">Planificar en vida</a></li>
+						<li>
+							<a href="<?php echo home_url(); ?>/post-funeral">Qué hacer después</a>
+							<ul>
+								<li><a href="#">Trámites post-funeral</a></li>
+								<li><a href="#">Apoyo en el duelo</a></li>
+							</ul>
+						</li>
+						<li><a href="<?php echo home_url(); ?>/">Contáctanos</a></li>
 					</ul>
 				</nav>
 			</div>
