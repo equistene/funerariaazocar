fd2335cbd8b0d83d58ce15164807ce16c3966d15
Cristian Berrios <cristian@ilogica.cl>
2023-08-07 19:43:10 -0400

    fixes

--- footer.php
+++ footer.php
@@ -4,45 +4,44 @@
 
 <?php do_action( 'tailpress_content_after' ); ?>
 
-<footer class="bg-gray-200 text-gray-900 py-10">
 
-	<div class="container mx-auto">
-		<div class="flex items-center gap-4 flex-col md:flex-row">
+<footer class="text-[#444]">
+	<div class="bg-gray-100">
+		<div class="container mx-auto flex justify-between items-end gap-8 py-6">
 			<div class="logo">
-				<img src="https://funerariaazocar.cl/wp-content/uploads/2017/12/logo-funeraria.png" alt="">
+				<img src="<?php bloginfo('template_url'); ?>/images/logo-horizontal.png" alt="Funeraria Azócar" class="w-auto max-h-12 md:max-h-20">
 			</div>
-			<div class="contents flex-1">
-				<div class="flex justify-start gap-2">
-					<a href="#">Términos y condiciones</a>
-					<a href="#">Términos y condiciones</a>
-					<a href="#">Términos y condiciones</a>
-				</div>
-				<div>
-					<p>Funeraria Azócar - 2023</p>
-				</div>
-				<div class="social flex justify-end gap-4">
-					<a href="#">
-						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
-						</svg>
-					</a>
-					<a href="#">
-						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
-						</svg>
-					</a>
-					<a href="#">
-						<svg class="h-4 w-auto" width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-							<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
-						</svg>
-					</a>
-				</div>
-
+			<div class="data flex-1 flex justify-between gap-4 items-start">
+				
 			</div>
 		</div>
-		
-		
+	</div>
+	<div class="container mx-auto flex justify-between items-center gap-4 py-4">		
+		<div class="data flex-1 pb-1">
+			<nav class="nav-footer">
+				<ul>
+					<li><a href="#">Nosotros</a></li>
+					<li><a href="#">Nosotros</a></li>
+					<li><a href="#">Nosotros</a></li>
+					<li><a href="#">Nosotros</a></li>
+					<li><a href="#">Nosotros</a></li>
+				</ul>
+			</nav>
+			<p class="pt-2 text-xs font-semibold">Funeraria Azócar - 2023</p>
+		</div>
+		<div class="sociales max-w-max">
+			<a href="#">
+				<svg width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path fill="#000000" d="M13.028 2.001a78.82 78.82 0 0 1 2.189.022l.194.007c.224.008.445.018.712.03c1.064.05 1.79.218 2.427.465c.66.254 1.216.598 1.772 1.154a4.908 4.908 0 0 1 1.153 1.771c.247.637.415 1.364.465 2.428c.012.266.022.488.03.712l.006.194a79 79 0 0 1 .023 2.188l.001.746v1.31a78.836 78.836 0 0 1-.023 2.189l-.006.194c-.008.224-.018.445-.03.712c-.05 1.064-.22 1.79-.466 2.427a4.884 4.884 0 0 1-1.153 1.772a4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.427.465c-.267.012-.488.022-.712.03l-.194.006a79 79 0 0 1-2.189.023l-.746.001h-1.309a78.836 78.836 0 0 1-2.189-.023l-.194-.006a60.64 60.64 0 0 1-.712-.03c-1.064-.05-1.79-.22-2.428-.466a4.89 4.89 0 0 1-1.771-1.153a4.904 4.904 0 0 1-1.154-1.772c-.247-.637-.415-1.363-.465-2.427a74.367 74.367 0 0 1-.03-.712l-.005-.194A79.053 79.053 0 0 1 2 13.028v-2.056a78.82 78.82 0 0 1 .022-2.188l.007-.194c.008-.224.018-.446.03-.712c.05-1.065.218-1.79.465-2.428A4.88 4.88 0 0 1 3.68 3.68a4.897 4.897 0 0 1 1.77-1.155c.638-.247 1.363-.415 2.428-.465l.712-.03l.194-.005A79.053 79.053 0 0 1 10.972 2h2.056Zm-1.028 5A5 5 0 1 0 12 17a5 5 0 0 0 0-10Zm0 2A3 3 0 1 1 12.001 15a3 3 0 0 1 0-6Zm5.25-3.5a1.25 1.25 0 0 0 0 2.498a1.25 1.25 0 0 0 0-2.5Z"/>
+				</svg>
+			</a>
 
+			<a href="#">
+				<svg width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+					<path fill="#000000" d="M22 12c0-5.52-4.48-10-10-10S2 6.48 2 12c0 4.84 3.44 8.87 8 9.8V15H8v-3h2V9.5C10 7.57 11.57 6 13.5 6H16v3h-2c-.55 0-1 .45-1 1v2h3v3h-3v6.95c5.05-.5 9-4.76 9-9.95z"/>
+				</svg>
+			</a>			
+		</div>
 	</div>
 
 </footer>
--- header.php
+++ header.php
@@ -27,12 +27,12 @@
 			<svg width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="h-4 w-auto">
 					<path fill="#ffffff" d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24c1.12.37 2.33.57 3.57.57c.55 0 1 .45 1 1V20c0 .55-.45 1-1 1c-9.39 0-17-7.61-17-17c0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1c0 1.25.2 2.45.57 3.57c.11.35.03.74-.25 1.02l-2.2 2.2z"/>
 			</svg>
-			<p class="text-white text-sm font-semibold uppercase">¿Cómo podemos ayudarte? Llámanos</p>
+			<p class="text-white text-sm font-semibold uppercase gotham-bold">¿Cómo podemos ayudarte? Llámanos</p>
 		</button>
-		<div class="container mx-auto flex justify-between items-center md:items-start">
+		<div class="container mx-auto flex justify-between items-center">
 			<div class="logo py-2">
 				<a href="<?php echo home_url(); ?>" class="block hover:opacity-60 transition-all">
-					<img src="<?php bloginfo('template_url'); ?>/images/logo-horizontal.png" alt="Funeraria Azócar" class="w-auto max-h-16 md:max-h-20">
+					<img src="<?php bloginfo('template_url'); ?>/images/logo-horizontal.png" alt="Funeraria Azócar" class="w-auto max-h-12 md:max-h-20">
 				</a>
 			</div>
 
