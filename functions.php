bb12eff815d92f7c8c7b71383707add4e546017f
Cristian Berrios <equistene@gmail.com>
2023-10-11 00:36:44 -0300

    avances

--- functions.php
+++ functions.php
@@ -95,6 +95,10 @@ function tailpress_nav_menu_add_li_class( $classes, $item, $args, $depth ) {
 	return $classes;
 }
 
+// add_filter( 'acf-flexible-content-extended.images_path', $path );
+
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
 add_filter( 'nav_menu_css_class', 'tailpress_nav_menu_add_li_class', 10, 4 );
 
 /**
@@ -132,3 +136,40 @@ function my_acf_json_save_point( $path ) {
     return $path;
     
 }
+
+// Allow SVG
+add_filter( 'wp_check_filetype_and_ext', function($data, $file, $filename, $mimes) {
+
+	global $wp_version;
+	if ( $wp_version !== '4.7.1' ) {
+	   return $data;
+	}
+  
+	$filetype = wp_check_filetype( $filename, $mimes );
+  
+	return [
+		'ext'             => $filetype['ext'],
+		'type'            => $filetype['type'],
+		'proper_filename' => $data['proper_filename']
+	];
+  
+  }, 10, 4 );
+  
+  function cc_mime_types( $mimes ){
+	$mimes['svg'] = 'image/svg+xml';
+	return $mimes;
+  }
+  add_filter( 'upload_mimes', 'cc_mime_types' );
+  
+  function fix_svg() {
+	echo '<style type="text/css">
+		  .attachment-266x266, .thumbnail img {
+			   width: 100% !important;
+			   height: auto !important;
+		  }
+		  </style>';
+  }
+  add_action( 'admin_head', 'fix_svg' );
+
+
+  
--- template-parts/contact-box.php
+++ template-parts/contact-box.php
@@ -10,9 +10,7 @@
         <?php while( have_rows('bloques_contacto', 'option') ): the_row(); ?>
             <div class="box box-content">
               <div class="icon">
-                <svg width="512" height="512" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path fill="#000000" d="M19.95 21q-3.125 0-6.175-1.363t-5.55-3.862q-2.5-2.5-3.862-5.55T3 4.05q0-.45.3-.75t.75-.3H8.1q.35 0 .625.238t.325.562l.65 3.5q.05.4-.025.675T9.4 8.45L6.975 10.9q.5.925 1.187 1.787t1.513 1.663q.775.775 1.625 1.438T13.1 17l2.35-2.35q.225-.225.588-.338t.712-.062l3.45.7q.35.1.575.363T21 15.9v4.05q0 .45-.3.75t-.75.3Z"/>
-                </svg>
+                <img src="<?php the_sub_field('icono') ; ?>" alt="Icono">
               </div>
               <p><?php the_sub_field('titulo'); ?></p>
 
